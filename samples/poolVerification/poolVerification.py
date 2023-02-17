ea8d3490e3bda8f674b5a0ccea1529642b2f3971
eYdr1en <54525514+eYdr1en@users.noreply.github.com>
2023-02-17 22:59:08 +0100

    add auroratestnet

--- samples/poolVerification/poolVerification.py
+++ samples/poolVerification/poolVerification.py
@@ -1,28 +1,30 @@
+#!/usr/bin/env python
 import balpy
 
 def main():
-	network = "goerli"
-	poolId = "0xe867ad0a48e8f815dc0cda2cdb275e0f163a480b0002000000000000000001a0"
-	# On Ethereum and Ethereum testnets, you can pass creationHash=None
-	# On Polygon, you must pass the pool creation hash to generate verification params
-	creationHash = None;
-	# creationHash = "0x18c7e1c9235c6e93878e55a87ed249f9d0ceb9d12ee584794e92f80f7645686d";
-	verbose = False;
+    network = "aurora"
+    poolId = "0xaed2a4589f21f3ad20ca8e389701579f15821466000200000000000000000001"
+    # On Ethereum and Ethereum testnets, you can pass creationHash=None
+    # On Polygon, you must pass the pool creation hash to generate verification params
+    creationHash = None;
+    creationHash = "0x249bea8a725ac04ec96505d6509c8e31b41605ce0f8a8e67a49f569ef97761ed";
+    verbose = True;
 
-	bal = balpy.balpy.balpy(network);
-	isVerified = bal.isContractVerified(poolId, verbose=verbose);
-	if not isVerified:
-		command = bal.balGeneratePoolCreationArguments(poolId, verbose=verbose, creationHash=creationHash);
-	else:
-		print("Pool is already verified")
-		quit();
-	
-	print()
-	print(command)
-	print()
+    bal = balpy.balpy.balpy(network);
+    # isVerified = bal.isContractVerified(poolId, verbose=verbose);
+    isVerified = False
+    if not isVerified:
+        command = bal.balGeneratePoolCreationArguments(poolId, verbose=verbose, creationHash=creationHash);
+    else:
+        print("Pool is already verified")
+        quit();
+    
+    print()
+    print(command)
+    print()
 
-	print("If you need more complete instructions on what to do with this command, go to:");
-	print("\thttps://dev.balancer.fi/resources/pools/verification\n");
-	
+    print("If you need more complete instructions on what to do with this command, go to:");
+    print("\thttps://dev.balancer.fi/resources/pools/verification\n");
+    
 if __name__ == '__main__':
-	main();
+    main();
